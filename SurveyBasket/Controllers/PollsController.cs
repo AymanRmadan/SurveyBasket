a4e913162267137d0b0a0d9744e2cd8d507e7ded
Ayman Ramadan <aymanramadan677@gmail.com>
2025-05-24 10:59:58 +0300

    Create poll controller and add get and get by id poll actions

---
+++ SurveyBasket/Controllers/PollsController.cs
@@ -0,0 +1,40 @@
+﻿namespace SurveyBasket.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PollsController : ControllerBase
+    {
+        private readonly List<Poll> _polls =
+            [
+            new Poll
+            {
+                Id = 1,
+                Title="Poll 1",
+                Description="My first Poll"
+            },
+              new Poll
+            {
+                Id = 2,
+                Title="Poll 2",
+                Description="My second Poll"
+            }
+
+            ];
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(_polls);
+        }
+
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var poll = _polls.SingleOrDefault(pol => pol.Id == id);
+            return poll is null ? NotFound() : Ok(poll);
+        }
+
+    }
+}
