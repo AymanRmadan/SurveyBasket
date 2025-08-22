e706d1b96312f507d832354f7349592b6c1de0f9
Ayman Ramadan <aymanramadan677@gmail.com>
2025-08-22 11:38:27 +0300

    Add complex api to get users with roles

--- SurveyBasket/Services/IUserService.cs
+++ SurveyBasket/Services/IUserService.cs
@@ -1,10 +1,12 @@
-﻿using SurveyBasket.Contracts.Users.Requests;
+﻿using SurveyBasket.Contracts.Users;
+using SurveyBasket.Contracts.Users.Requests;
 using SurveyBasket.Contracts.Users.Responses;
 
 namespace SurveyBasket.Services
 {
     public interface IUserService
     {
+        Task<IEnumerable<UserResponse>> GetAllAsync(CancellationToken cancellationToken = default);
         public Task<Result<UserProfileResponse>> GetProfileAsync(string userId);
         public Task<Result> UpdateProfileAsync(string userId, UpdateProfileRequest request);
 
--- SurveyBasket/Services/UserService.cs
+++ SurveyBasket/Services/UserService.cs
@@ -1,18 +1,47 @@
 ﻿using Mapster;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using SurveyBasket.Abstractions.Consts;
+using SurveyBasket.Contracts.Users;
 using SurveyBasket.Contracts.Users.Requests;
 using SurveyBasket.Contracts.Users.Responses;
+using SurveyBasket.Persistence;
 
 namespace SurveyBasket.Services
 {
 
-    public class UserService(UserManager<ApplicationUser> userManager) : IUserService
+    public class UserService(UserManager<ApplicationUser> userManager, AppDbContext context) : IUserService
     {
         private readonly UserManager<ApplicationUser> _userManager = userManager;
+        private readonly AppDbContext _context = context;
 
-
-
+        public async Task<IEnumerable<UserResponse>> GetAllAsync(CancellationToken cancellationToken = default) =>
+    await (from u in _context.Users
+           join ur in _context.UserRoles
+           on u.Id equals ur.UserId
+           join r in _context.Roles
+           on ur.RoleId equals r.Id into roles
+           where !roles.Any(x => x.Name == DefaultRoles.Member)
+           select new
+           {
+               u.Id,
+               u.FirstName,
+               u.LastName,
+               u.Email,
+               u.IsDisabled,
+               Roles = roles.Select(x => x.Name!).ToList()
+           }
+            )
+            .GroupBy(u => new { u.Id, u.FirstName, u.LastName, u.Email, u.IsDisabled })
+            .Select(u => new UserResponse
+            (
+                u.Key.Id,
+                u.Key.FirstName,
+                u.Key.LastName,
+                u.Key.Email,
+                u.Key.IsDisabled,
+                u.SelectMany(x => x.Roles)
+            ))
+           .ToListAsync(cancellationToken);
         public async Task<Result<UserProfileResponse>> GetProfileAsync(string userId)
         {
             //var user = await _userManager.FindByIdAsync(userId);
